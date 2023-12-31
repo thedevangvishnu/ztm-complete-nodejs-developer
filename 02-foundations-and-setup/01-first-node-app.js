2973a65bf50b504cd5aa2c65fe257c495f88aff3
Devang Vishnu <thedevangvishnu@gmail.com>
2023-12-31 17:35:52 +0530

    Create a basic nodejs app
    Write a basic nodejs program for the first app.
    For the second one, use the same program but take inputs using
    process.argv

---
+++ 02-foundations-and-setup/01-first-node-app.js
@@ -0,0 +1,5 @@
+const mission = "learn";
+
+mission === "learn"
+  ? console.log("Time to write some node!")
+  : console.log(`Is ${mission} really more fun`);
---
+++ 02-foundations-and-setup/02-node-app-using-argv.js
@@ -0,0 +1,8 @@
+const mission = process.argv[2];
+
+mission === "learn"
+  ? console.log("Time to write some node!")
+  : console.log(`Is ${mission} really more fun?`);
+
+// provide "explore" as input
+// provide "learn" as input
