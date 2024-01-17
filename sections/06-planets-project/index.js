369e168f4376b76e673d448f248d0d6e24c7cabc
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-17 11:58:28 +0530

    Write the program to find habitable planets from kepler data
    Using the kepler-data, write a program a index.js using the fs and
    csv-parse module and also using the concept of streaming to find
    all the habitable planets.

--- sections/06-planets-project/index.js
+++ sections/06-planets-project/index.js
@@ -1,1 +1,33 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const { platform } = require("os");
 
+const habitablePlanets = [];
+const parser = parse({
+  comment: "#",
+  columns: true,
+});
+
+const isHabitable = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.37 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+fs.createReadStream("./kepler-data.csv")
+  .pipe(parser)
+  .on("data", (data) => {
+    if (isHabitable(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log("Following are the habitable planets:");
+    habitablePlanets.forEach((planet) => console.log(planet["kepler_name"]));
+  });
