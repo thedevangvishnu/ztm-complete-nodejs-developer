45c8c0e943b5486760f799d42babc82be4c0e3b7
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-17 19:18:08 +0530

    Create post route and use middleware

--- sections/08-express-api/01-first-express-app.js
+++ sections/08-express-api/01-first-express-app.js
@@ -1,9 +1,15 @@
 // Create a simple express server.
+// In the freinds database,
+// create a get route that get all the users
+// create a get route that gets the specific user based on the id (passed as request param)
+// create a post route to add new user to the database
+// create a put route that can update the existing user on their matching id
+// create a delete route that can delete user from the database
 
 const express = require("express");
 const app = express();
 
-const friends = [
+const users = [
   {
     id: 1,
     name: "Shivam",
@@ -22,33 +28,48 @@ const friends = [
   },
 ];
 
+app.use(express.json());
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Home Page</h1>");
 });
 
-app.get("/friends", (req, res) => {
-  res.status(200).json(friends);
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
 });
 
-app.get("/friends/:id", (req, res) => {
+app.get("/users/:id", (req, res) => {
   const requestId = Number(req.params.id);
 
-  const friendProfile = friends[requestId - 1];
-  console.log(friendProfile);
+  const userProfile = users[requestId - 1];
+  console.log(userProfile);
 
-  if (friendProfile) {
-    res.status(200).json(friendProfile);
+  if (userProfile) {
+    res.status(200).json(userProfile);
   } else {
     res.status(404).json("Not found");
   }
 });
 
-app.get("/user", (req, res) => {
-  res.json({
-    id: 1,
-    name: "Devang Vishnu",
-    numberOfPosts: 25,
-  });
+app.post("/newuser", (req, res) => {
+  if (!req.body.name) {
+    return res.status(403).json({ error: "name is missing" });
+  }
+
+  const user = {
+    id: users.length + 1,
+    name: req.body.name,
+  };
+
+  users.push(user);
+  res.status(200).json({ msg: "New user added successfully", newuser: user });
 });
 
 app.listen(3000);
