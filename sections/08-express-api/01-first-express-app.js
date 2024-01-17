b4894630496959fe00f8e54a34e68de3b9f0f83d
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-17 18:29:27 +0530

    Write the express server that has basic endpoints

--- sections/08-express-api/01-first-express-app.js
+++ sections/08-express-api/01-first-express-app.js
@@ -1,1 +1,54 @@
 // Create a simple express server.
+
+const express = require("express");
+const app = express();
+
+const friends = [
+  {
+    id: 1,
+    name: "Shivam",
+  },
+  {
+    id: 2,
+    name: "Rohit",
+  },
+  {
+    id: 3,
+    name: "Priya",
+  },
+  {
+    id: 4,
+    name: "Ankit",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Home Page</h1>");
+});
+
+app.get("/friends", (req, res) => {
+  res.status(200).json(friends);
+});
+
+app.get("/friends/:id", (req, res) => {
+  const requestId = Number(req.params.id);
+
+  const friendProfile = friends[requestId - 1];
+  console.log(friendProfile);
+
+  if (friendProfile) {
+    res.status(200).json(friendProfile);
+  } else {
+    res.status(404).json("Not found");
+  }
+});
+
+app.get("/user", (req, res) => {
+  res.json({
+    id: 1,
+    name: "Devang Vishnu",
+    numberOfPosts: 25,
+  });
+});
+
+app.listen(3000);
