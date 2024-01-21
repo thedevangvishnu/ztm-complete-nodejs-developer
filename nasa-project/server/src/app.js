5d545659e965423f1a862ed025b7412c0319d28c
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-21 10:10:35 +0530

    Run build in client and use static files in server

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,8 +1,11 @@
+const path = require("path");
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.routes");
 const cors = require("cors");
 
 const app = express();
+const publicPath = path.join(__dirname, "..", "public");
+const indexFilePath = path.join(__dirname, "..", "public", "index.html");
 
 app.use(express.json());
 app.use(
@@ -11,5 +14,10 @@ app.use(
   })
 );
 app.use(planetsRouter);
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(indexFilePath);
+});
 
 module.exports = app;
