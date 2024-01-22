d6729ab49911fed70d6e28f34ae4b9236541525e
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-22 10:58:34 +0530

    Add the logic to get launches in httpGetLaunches()
    In the requests.js, in httpGetLaunches(), add the logic that gets
    all the laucnhes data and sorts them in ascending order according
    to their flight number.

--- nasa-project/client/src/hooks/requests.js
+++ nasa-project/client/src/hooks/requests.js
@@ -8,7 +8,13 @@ async function httpGetPlanets() {
 }
 
 // Load launches, sort by flight number, and return as JSON.
-async function httpGetLaunches() {}
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`);
+  const launches = await response.json();
+  return launches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
 
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {}
