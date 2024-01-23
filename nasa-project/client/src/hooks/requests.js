3da23af4f62fb222640b30ffd18bcc07f4969e9a
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-23 12:53:04 +0530

    Add logic into httpSubmitLaunch()
    Add the logic into httpSubmitLaunch(launch) function
    that successfully posts new launches by taking in all
    the neccessary data from useLaunches.js

--- nasa-project/client/src/hooks/requests.js
+++ nasa-project/client/src/hooks/requests.js
@@ -17,7 +17,23 @@ async function httpGetLaunches() {
 }
 
 // Submit given launch data to launch system.
-async function httpSubmitLaunch(launch) {}
+async function httpSubmitLaunch(launch) {
+  try {
+    const response = await fetch(`${API_URL}/launches`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+
+    return response;
+  } catch (error) {
+    return {
+      ok: false,
+    };
+  }
+}
 
 // Delete launch with given ID.
 async function httpAbortLaunch(id) {}
