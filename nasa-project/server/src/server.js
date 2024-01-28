2ad721e9cc63ac078a523fff2fef5fc2ad8577e7
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-28 16:40:03 +0530

    Install mongoose and connect server to mongoDb cluster

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,11 +1,24 @@
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
 const { loadPlanetsData } = require("./models/planets/planets.model");
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("Mongoose connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(
+    "mongodb+srv://dv080499:hesPL31sebkUXATp@cluster0.vjkszou.mongodb.net/nasa-project"
+  );
+
   // load the planett data and then start the server
   await loadPlanetsData();
 
