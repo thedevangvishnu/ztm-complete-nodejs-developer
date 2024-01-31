c6c745c920c34b2fc716b707400b8033b1010b1f
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-31 12:52:17 +0530

    Install passport and dotenv and configure client id and secret

--- sections/12-security-and-authentication/01-security-example/server.js
+++ sections/12-security-and-authentication/01-security-example/server.js
@@ -4,6 +4,13 @@ const fs = require("fs");
 const express = require("express");
 const helmet = require("helmet");
 
+require("dotenv").config();
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
 const app = express();
 const server = https.createServer(
   {
