8ad203865ce81ab5332b025b9dffb12e646d2015
Devang Vishnu <thedevangvishnu@gmail.com>
2024-03-13 09:46:52 +0530

    Use passport to implement Google sign in

--- sections/12-security-and-authentication/01-security-example/server.js
+++ sections/12-security-and-authentication/01-security-example/server.js
@@ -3,13 +3,27 @@ const path = require("path");
 const fs = require("fs");
 const express = require("express");
 const helmet = require("helmet");
-
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 require("dotenv").config();
 
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
+
+const AUTH_OPTIONS = {
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+  callbackURL: "/auth/google/callback",
+};
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 const server = https.createServer(
@@ -32,17 +46,39 @@ const checkLoggedIn = (req, res, next) => {
 };
 
 app.use(helmet());
+app.use(passport.initialize());
 
 app.get("/", (req, res) => {
   res.sendFile(indexPath);
 });
 
-app.get("/auth/google", (req, res) => {});
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back");
+  }
+);
+
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   res.json({ message: "You security key is 32" });
+});
+
+app.get("/failure", (req, res) => {
+  res.json({ error: "Failed to login" });
 });
 
 server.listen(PORT, () => {
